2ddac1db25ccecf072e9ccd1f66ecf8e918e53f9
Ruslan Adzhigitov <adzhigitovruslan@gmail.com>
2023-05-19 20:54:11 +0800

    feat: add settings component

---
+++ minesweeper/src/components/settings/index.js
@@ -0,0 +1,7 @@
+import htmlToElement from '../../utils/htmlToElement';
+import './index.scss';
+import Settings from './index.html';
+
+const settings = htmlToElement(Settings);
+
+export default settings;
